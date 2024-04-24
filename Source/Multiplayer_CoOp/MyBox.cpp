aa3ccc9c99b400a9f6d32d1ecd86ca7373eccc80
Dominic London <londonrdominic@gmail.com>
2024-04-24 01:06:36 -0700

    Spawning Particle Effect

--- Source/Multiplayer_CoOp/MyBox.cpp
+++ Source/Multiplayer_CoOp/MyBox.cpp
@@ -3,6 +3,7 @@
 
 #include "MyBox.h"
 #include "Net/UnrealNetwork.h"
+#include "Kismet/GameplayStatics.h"
 
 // Sets default values
 AMyBox::AMyBox()
@@ -98,5 +99,12 @@ void AMyBox::MulticastRPCExplode_Implementation()
 	{
 		GEngine->AddOnScreenDebugMessage(-1, 3.0f, FColor::Green,
 			TEXT("Client: MulticastRPCExplode_Implementation"));
+	}
+
+	if (!IsRunningDedicatedServer())
+	{
+		FVector SpawnLocation = GetActorLocation() + FVector(0, 0, 100.0f);
+		UGameplayStatics::SpawnEmitterAtLocation(GetWorld(), ExplosionEffect, SpawnLocation,
+			FRotator::ZeroRotator, true, EPSCPoolMethod::AutoRelease);
 	}
 }
